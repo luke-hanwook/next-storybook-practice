440373a8db0ea298aad74ea64b5c8ed76697d068
Luke <gksdnr89@gmail.com>
2020-08-01 02:53:10 +0900

    storybook ts 적용

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../stories/**/*.stories.tsx"],
+  addons: [
+    "@storybook/preset-typescript",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+  ],
+};
